4ce219233f7454bd3339a63e2913669b77cef22b
zlamprich <zlamprich@users.noreply.github.com>
2022-07-05 17:54:29 +0000

    reviewing how to manipulate JSON data with python

---
+++ dict/dict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+## create a dictionary
+goku = {"Real name": "Kakarot", "age": "43", "Saiyan levels":[ "1","2","3","4","God","Blue"], "muscles": "tons of them"}
+
+## display parts of the dictionary
+print( goku["age"] )
+print( goku["Real name"] )
+print( goku["Saiyan levels"] )
+  
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,5 @@
+#! /user/bin/env python3
+my_list = ["192.168.0.5", 5060, "UP"]
+print("The first item in the list (IP): " + my_list[0])
+cd
+q
